786c14c2a28aed250db9bea876e159153d0707b5
Petro <petdavy@1991gmail.com>
2020-10-06 17:28:33 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  if (Object.keys(data).length) {
+    const list = document.createElement('ul');
+
+    element.append(list);
+
+    for (const key in data) {
+      const listItem = document.createElement('li');
+
+      listItem.textContent = key;
+      list.append(listItem);
+      createTree(listItem.parentElement, data[key]);
+    }
+  }
+}
+
+createTree(tree, food);
